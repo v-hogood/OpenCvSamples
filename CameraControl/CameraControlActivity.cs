5441d2e7103be44c3f025a47361e5bb81ef1add8
Howard Good <v-hogood@outlook.com>
2023-05-16 21:58:15 -0700

    Add CameraControl sample.

---
+++ CameraControl/CameraControlActivity.cs
@@ -0,0 +1,207 @@
+using System.Collections.Generic;
+using Android.App;
+using Android.OS;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Java.Text;
+using Java.Util;
+using OpenCV.Android;
+using OpenCV.Core;
+using static OpenCV.Android.CameraBridgeViewBase;
+using Size = Android.Hardware.Camera.Size;
+
+namespace CameraControl
+{
+    [Activity(Name = "org.opencv.samples.cameracontrol.CameraControlActivity", Label = "@string/app_name", Theme = "@android:style/Theme.NoTitleBar.Fullscreen", MainLauncher = true)]
+    public class CameraControlActivity : CameraActivity,
+        CameraBridgeViewBase.ICvCameraViewListener2,
+        View.IOnTouchListener
+    {
+        private const string Tag = "OCVSample::Activity";
+
+        private CameraControlView mOpenCvCameraView;
+        private IList<Size> mResolutionList;
+        private IMenu mMenu;
+        private bool mCameraStarted = false;
+        private bool mMenuItemsCreated = false;
+        private IMenuItem[] mEffectMenuItems;
+        private ISubMenu mColorEffectsMenu;
+        private IMenuItem[] mResolutionMenuItems;
+        private ISubMenu mResolutionMenu;
+
+        public class LoaderCallback : BaseLoaderCallback
+        {
+            public LoaderCallback(CameraControlActivity activity) : base(activity)
+            {
+                this.activity = activity;
+            }
+            CameraControlActivity activity;
+
+            override public void OnManagerConnected(int status)
+            {
+                switch (status)
+                {
+                    case LoaderCallbackInterface.Success:
+                        {
+                            Log.Info(Tag, "OpenCV loaded successfully");
+                            activity.mOpenCvCameraView.SetOnTouchListener(activity);
+                            activity.mOpenCvCameraView.EnableView();
+                        }
+                        break;
+                    default:
+                        {
+                            base.OnManagerConnected(status);
+                        }
+                        break;
+                }
+            }
+        }
+        private BaseLoaderCallback mLoaderCallback;
+
+        public CameraControlActivity()
+        {
+            Log.Info(Tag, "Instantiated new " + this.Class);
+            mLoaderCallback = new LoaderCallback(this);
+        }
+
+        // Called when the activity is first created.
+        override protected void OnCreate(Bundle savedInstanceState)
+        {
+            Log.Info(Tag, "called OnCreate");
+            base.OnCreate(savedInstanceState);
+            Window.AddFlags(WindowManagerFlags.KeepScreenOn);
+
+            SetContentView(Resource.Layout.cameracontrol_surface_view);
+
+            mOpenCvCameraView = FindViewById<CameraControlView>(Resource.Id.cameracontrol_activity_java_surface_view);
+
+            mOpenCvCameraView.Visibility = ViewStates.Visible;
+
+            mOpenCvCameraView.SetCvCameraViewListener2(this);
+        }
+
+        override protected void OnPause()
+        {
+            base.OnPause();
+            if (mOpenCvCameraView != null)
+                mOpenCvCameraView.DisableView();
+        }
+
+        override protected void OnResume()
+        {
+            base.OnResume();
+            if (!OpenCVLoader.InitDebug())
+            {
+                Log.Debug(Tag, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+                OpenCVLoader.InitAsync(OpenCVLoader.OpencvVersion300, this, mLoaderCallback);
+            }
+            else
+            {
+                Log.Debug(Tag, "OpenCV library found inside package. Using it!");
+                mLoaderCallback.OnManagerConnected(LoaderCallbackInterface.Success);
+            }
+        }
+
+        override protected IList<CameraBridgeViewBase> CameraViewList =>
+            new List<CameraBridgeViewBase>(1) { mOpenCvCameraView };
+
+        override protected void OnDestroy()
+        {
+            base.OnDestroy();
+            if (mOpenCvCameraView != null)
+                mOpenCvCameraView.DisableView();
+        }
+
+        public void OnCameraViewStarted(int width, int height)
+        {
+            mCameraStarted = true;
+            SetupMenuItems();
+        }
+
+        public void OnCameraViewStopped() { }
+
+        public Mat OnCameraFrame(ICvCameraViewFrame inputFrame)
+        {
+            return inputFrame.Rgba();
+        }
+
+        override public bool OnCreateOptionsMenu(IMenu menu)
+        {
+            mMenu = menu;
+            SetupMenuItems();
+            return true;
+        }
+
+        private void SetupMenuItems()
+        {
+            if (mMenu == null || !mCameraStarted || mMenuItemsCreated)
+            {
+                return;
+            }
+            IList<string> effects = mOpenCvCameraView.EffectList;
+
+            if (effects == null)
+            {
+                Log.Error(Tag, "Color effects are not supported by device!");
+                return;
+            }
+
+            mColorEffectsMenu = mMenu.AddSubMenu("Color Effect");
+            mEffectMenuItems = new IMenuItem[effects.Count];
+
+            int idx = 0;
+            foreach(string effect in effects)
+            {
+                mEffectMenuItems[idx] = mColorEffectsMenu.Add(1, idx, IMenu.None, effect);
+                idx++;
+            }
+
+            mResolutionMenu = mMenu.AddSubMenu("Resolution");
+            mResolutionList = mOpenCvCameraView.ResolutionList;
+            mResolutionMenuItems = new IMenuItem[mResolutionList.Count];
+
+            idx = 0;
+            foreach(Size resolution in mResolutionList)
+            {
+                mResolutionMenuItems[idx] = mResolutionMenu.Add(2, idx, IMenu.None,
+                        resolution.Width + "x" + resolution.Height);
+                idx++;
+            }
+            mMenuItemsCreated = true;
+        }
+
+        override public bool OnOptionsItemSelected(IMenuItem item)
+        {
+            Log.Info(Tag, "called onOptionsItemSelected; selected item: " + item);
+            if (item.GroupId == 1)
+            {
+                mOpenCvCameraView.Effect = item.TitleFormatted.ToString();
+                Toast.MakeText(this, mOpenCvCameraView.Effect, ToastLength.Short).Show();
+            }
+            else if (item.GroupId == 2)
+            {
+                int id = item.ItemId;
+                Size resolution = mResolutionList[id];
+                mOpenCvCameraView.Resolution = resolution;
+                resolution = mOpenCvCameraView.Resolution;
+                string caption = resolution.Width + "x" + resolution.Height;
+                Toast.MakeText(this, caption, ToastLength.Short).Show();
+            }
+
+            return true;
+        }
+
+        public bool OnTouch(View v, MotionEvent e)
+        {
+            Log.Info(Tag,"OnTouch event");
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+            string currentDateandTime = sdf.Format(new Date());
+            string fileName = Environment.ExternalStorageDirectory.Path +
+                               "/sample_picture_" + currentDateandTime + ".jpg";
+            mOpenCvCameraView.TakePicture(fileName);
+            Toast.MakeText(this, fileName + " saved", ToastLength.Short).Show();
+            return false;
+        }
+    }
+}
---
+++ CameraControl/CameraControlView.cs
@@ -0,0 +1,87 @@
+using System.Collections.Generic;
+using Android.Content;
+using Android.Util;
+using Java.IO;
+using OpenCV.Android;
+using Camera = Android.Hardware.Camera;
+using Size = Android.Hardware.Camera.Size;
+
+namespace CameraControl
+{
+    public class CameraControlView : JavaCameraView,
+        Camera.IPictureCallback
+    {
+        private const string Tag = "Sample::CameraControlView";
+        private string mPictureFileName;
+
+        public CameraControlView(Context context, IAttributeSet attrs) :
+            base(context, attrs)
+        { }
+
+        public IList<string> EffectList =>
+            MCamera.GetParameters().SupportedColorEffects;
+
+        public bool IsEffectSupported =>
+            (MCamera.GetParameters().ColorEffect != null);
+
+        public string Effect
+        {
+            get => MCamera.GetParameters().ColorEffect;
+            set
+            {
+                Camera.Parameters parms = MCamera.GetParameters();
+                parms.ColorEffect = value;
+                MCamera.SetParameters(parms);
+            }
+        }     
+
+        public IList<Size> ResolutionList =>
+            MCamera.GetParameters().SupportedPreviewSizes;
+
+        public Size Resolution
+        {
+            get => MCamera.GetParameters().PreviewSize;
+            set
+            {
+                DisconnectCamera();
+                MMaxHeight = value.Height;
+                MMaxWidth = value.Width;
+                ConnectCamera(Width, Height);
+            }
+        }
+
+        public void TakePicture(string fileName)
+        {
+            Log.Info(Tag, "Taking picture");
+            this.mPictureFileName = fileName;
+            // Postview and jpeg are sent in the same buffers if the queue is not empty when performing a capture.
+            // Clear up buffers to avoid mCamera.takePicture to be stuck because of a memory issue
+            MCamera.SetPreviewCallback(null);
+
+            // PictureCallback is implemented by the current class
+            MCamera.TakePicture(null, null, this);
+        }
+
+        public void OnPictureTaken(byte[] data, Camera camera)
+        {
+            Log.Info(Tag, "Saving a bitmap to file");
+            // The camera preview was automatically stopped. Start it again.
+            MCamera.StartPreview();
+            MCamera.SetPreviewCallback(this);
+
+            // Write the image in a file (in jpeg format)
+            try
+            {
+                FileOutputStream fos = new FileOutputStream(mPictureFileName);
+
+                fos.Write(data);
+                fos.Close();
+
+            }
+            catch (IOException e)
+            {
+                Log.Error("PictureDemo", "Exception in photoCallback", e);
+            }
+        }
+    }
+}
